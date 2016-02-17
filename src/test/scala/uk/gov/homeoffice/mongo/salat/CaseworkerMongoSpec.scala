95871fefb2ef582185a3d5556974cd559ca82a89
David Ainslie <dainslie@gmail.com>
2016-02-17 15:46:31 +0000

    Using renamed specifications from mongo-lib

--- src/test/scala/uk/gov/homeoffice/mongo/salat/CaseworkerMongoSpec.scala
+++ src/test/scala/uk/gov/homeoffice/mongo/salat/CaseworkerMongoSpec.scala
@@ -3,9 +3,9 @@ package uk.gov.homeoffice.mongo.salat
 import org.specs2.execute.{AsResult, Result}
 import org.specs2.matcher.Scope
 import org.specs2.mutable.Specification
-import uk.gov.homeoffice.mongo.casbah.{CaseworkerMongo, EmbeddedMongoSpec}
+import uk.gov.homeoffice.mongo.casbah.{CaseworkerMongo, EmbeddedMongoSpecification}
 
-class CaseworkerMongoSpec extends Specification with EmbeddedMongoSpec {
+class CaseworkerMongoSpec extends Specification with EmbeddedMongoSpecification {
   override def around[T: AsResult](t: => T): Result = try {
     System.setProperty("caseworker.mongodb", s"mongodb://${mongoClient.connectPoint}/$database")
     super.around(t)
