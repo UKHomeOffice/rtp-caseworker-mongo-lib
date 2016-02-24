fe593c17d8a16241805ecf90768e5a2bd1b4df32
David Ainslie <dainslie@gmail.com>
2016-02-24 22:25:50 +0000

    Removed extra space

--- src/main/scala/uk/gov/homeoffice/mongo/casbah/CaseworkerMongo.scala
+++ src/main/scala/uk/gov/homeoffice/mongo/casbah/CaseworkerMongo.scala
@@ -17,5 +17,5 @@ trait CaseworkerMongo extends Mongo {
   * caseworker.mongodb = "mongodb://user:pass@host:27011,host2:27012,host3:27013/caseworker"
   */
 object CaseworkerMongo extends HasConfig {
-  lazy val caseworkerdb = Mongo.db(MongoClientURI(config.getString("caseworker.mongodb")) )
+  lazy val caseworkerdb = Mongo db MongoClientURI(config.getString("caseworker.mongodb"))
 }
