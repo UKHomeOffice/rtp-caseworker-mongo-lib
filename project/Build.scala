5a490d691b5737a799000254216166c7d957b4c8
Ryan Da Costa <Ryan.daCosta@digital.homeoffice.gov.uk>
2016-04-18 11:28:24 +0100

    RTP-573: Install & Use sbt-release plugin

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,6 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.5.3",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
