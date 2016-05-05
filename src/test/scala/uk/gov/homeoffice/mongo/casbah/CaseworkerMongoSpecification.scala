3dccdea8e503a2ec812358eecd06104edba388b0
David Ainslie <dainslie@gmail.com>
2016-05-05 17:59:13 +0100

    New trait with a mongoConfiguration that can be used by examples such as Play examples utilising WithApplication.

---
+++ src/test/scala/uk/gov/homeoffice/mongo/casbah/CaseworkerMongoSpecification.scala
@@ -0,0 +1,12 @@
+package uk.gov.homeoffice.mongo.casbah
+
+import org.specs2.mutable.SpecificationLike
+
+trait CaseworkerMongoSpecification extends EmbeddedMongoSpecification {
+  this: SpecificationLike =>
+
+  lazy val mongoConfiguration: Map[String, String] = Map(
+    "mongodb.uri" -> s"mongodb://localhost:${network.getPort}/$database",
+    "caseworker.mongodb" -> s"mongodb://localhost:${network.getPort}/$database",
+    "memcachedplugin" -> "disabled")
+}
