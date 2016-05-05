48eceedf8a48ed2276c457d51edee31176d6ed6a
David Ainslie <dainslie@gmail.com>
2016-05-05 12:23:37 +0100

    Upgraded dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -41,8 +41,8 @@ object Build extends Build {
     )
     .settings(libraryDependencies ++= {
       val `gatling-verson` = "2.1.7"
-      val `rtp-test-lib-version` = "1.2.4"
-      val `rtp-mongo-lib-version` = "1.7.6"
+      val `rtp-test-lib-version` = "1.3.1"
+      val `rtp-mongo-lib-version` = "1.7.7"
 
       Seq(
         "uk.gov.homeoffice" %% "rtp-test-lib" % `rtp-test-lib-version` withSources(),
