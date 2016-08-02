1deca32939e7995752af95f37a670d13a5394683
David Ainslie <dainslie@gmail.com>
2016-08-02 09:21:02 +0100

    Upgraded mongo-lib

--- project/Build.scala
+++ project/Build.scala
@@ -42,7 +42,7 @@ object Build extends Build {
     .settings(libraryDependencies ++= {
       val `gatling-verson` = "2.2.0"
       val `rtp-test-lib-version` = "1.3.1"
-      val `rtp-mongo-lib-version` = "1.7.17"
+      val `rtp-mongo-lib-version` = "1.7.18"
 
       Seq(
         "uk.gov.homeoffice" %% "rtp-test-lib" % `rtp-test-lib-version` withSources(),
