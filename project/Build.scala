88e5037d14d51ec3654940cf3222c1ddad40052a
David Ainslie <dainslie@gmail.com>
2016-11-10 14:19:14 +0000

    Updated to latest rtp-mongo-lib

--- project/Build.scala
+++ project/Build.scala
@@ -43,7 +43,7 @@ object Build extends Build {
     .settings(libraryDependencies ++= {
       val `gatling-verson` = "2.2.0"
       val `rtp-test-lib-version` = "1.3.4"
-      val `rtp-mongo-lib-version` = "3.0.0"
+      val `rtp-mongo-lib-version` = "3.0.1"
 
       Seq(
         "uk.gov.homeoffice" %% "rtp-test-lib" % `rtp-test-lib-version` withSources(),
