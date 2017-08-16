0d92afb3e5b8274b99ba6f56f969c6f10aaa70cd
Ryan Da Costa <Ryan.daCosta@digital.homeoffice.gov.uk>
2017-08-16 16:26:58 +0100

    [NIJ] - Update library dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -42,8 +42,8 @@ object Build extends Build {
     )
     .settings(libraryDependencies ++= {
       val `gatling-verson` = "2.2.0"
-      val `rtp-test-lib-version` = "1.3.4"
-      val `rtp-mongo-lib-version` = "3.0.10"
+      val `rtp-test-lib-version` = "1.4.2"
+      val `rtp-mongo-lib-version` = "3.0.11"
 
       Seq(
         "uk.gov.homeoffice" %% "rtp-test-lib" % `rtp-test-lib-version` withSources(),
